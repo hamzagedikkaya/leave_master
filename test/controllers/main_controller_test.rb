646a6f38481145479d40b1285fdf7b768583bcfc
hamzagedikkaya <gedikkayahamza@gmail.com>
2024-10-19 09:38:40 +0300

    refactor: remove redundant route and comment out home controller test

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class MainControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get main_home_url
-    assert_response :success
-  end
+  # test "should get home" do
+  #   get main_home_url
+  #   assert_response :success
+  # end
 end
