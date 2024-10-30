947361d488ec150ea97176857e5f2b4aeded32e4
hamzagedikkaya <gedikkayahamza@gmail.com>
2024-10-30 17:47:40 +0300

    feat: integrate Simple Form with Devise

--- Gemfile
+++ Gemfile
@@ -61,3 +61,5 @@ end
 gem "tailwindcss-rails", "~> 3.0"
 
 gem "devise", "~> 4.9"
+
+gem "simple_form", "~> 5.3"
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validates
+  validates :name_surname, presence: { message: I18n.t("errors.messages.name_surname_blank") },
+                           format: { with: /\A[a-zA-ZşŞıİçÇğĞöÖüÜ\s]+\z/, message: I18n.t("errors.messages.name_surname_invalid") }
+
+  validates :gsm, presence: { message: I18n.t("errors.messages.gsm_invalid") },
+                  numericality: { only_integer: true, message: I18n.t("errors.messages.gsm_invalid") }
+
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :date_of_birth, presence: true
 end
