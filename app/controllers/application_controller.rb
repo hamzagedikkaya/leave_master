457a8a92870e47e59adb853f438536dd41107593
bariskilic <baris.kilic@iwallet.com.tr>
2024-11-03 22:14:01 +0300

    feat: optimize locale setting for user sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@
 
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = current_user&.locale || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
