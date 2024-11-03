62147f145aa26e46e563102a5b4e71c86632f80b
hamzagedikkaya <gedikkayahamza@gmail.com>
2024-11-03 15:38:15 +0300

    chore: add schema to .gitignore and fix migration syntax

--- db/migrate/20241102140901_add_trackable_to_users.rb
+++ db/migrate/20241102140901_add_trackable_to_users.rb
@@ -3,11 +3,11 @@
 class AddTrackableToUsers < ActiveRecord::Migration[7.2]
   def change
     change_table :users, bulk: true do |t|
-      t.integer :sign_in_count
+      t.integer  :sign_in_count
       t.datetime :last_sign_in_at
-      t.string :last_sign_in_ip
+      t.string   :last_sign_in_ip
       t.datetime :current_sign_in_at
-      t.string :current_sign_in_ip
+      t.string   :current_sign_in_ip
     end
   end
 end
