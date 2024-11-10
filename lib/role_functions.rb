c2a0aa3567d09ef5f72c2d55ba933222a2d0520a
hamzagedikkaya <gedikkayahamza@gmail.com>
2024-11-10 15:56:36 +0300

    feat(migration): drop teams table with reversible migration

--- lib/role_functions.rb
+++ lib/role_functions.rb
@@ -3,18 +3,15 @@
 class RoleFunctions
   MENU_ABILITY_MODELS = [
     %w[Hesaplar users_path],
-    %w[Roller roles_path],
-    %w[Takımlar teams_path]
+    %w[Roller roles_path]
   ].freeze
 
   USER_ABILITY_MODELS = [%w[Görüntüleme index], %w[Oluşturma new], %w[Güncelleme create], %w[Düzenleme edit], %w[Güncelleme update], %w[Silme destroy]].freeze
   ROLE_ABILITY_MODELS = [%w[Görüntüleme index], %w[Oluşturma new], %w[Güncelleme create], %w[Düzenleme edit], %w[Güncelleme update], %w[Silme destroy]].freeze
-  TEAM_ABILITY_MODELS = [%w[Görüntüleme index], %w[Oluşturma new], %w[Güncelleme create], %w[Düzenleme edit], %w[Güncelleme update], %w[Silme destroy]].freeze
 
   ALL_ABILITY_MODELS = {
     "users_path" => USER_ABILITY_MODELS,
-    "roles_path" => ROLE_ABILITY_MODELS,
-    "teams_path" => TEAM_ABILITY_MODELS
+    "roles_path" => ROLE_ABILITY_MODELS
   }.freeze
 
   def self.get_abilities_for_path(path)
